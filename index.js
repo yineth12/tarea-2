d6b7e0a1dc495e5837076577d7cd964c7d04ac0a
Yinel Ayala <>
2021-04-27 04:33:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//programa que indique si  es mayor de edad// 
+let nombre=prompt("indique su nombre")
+let edad=parseInt(prompt("indique la edad"));
+
+let edad2 = 18
+if (edad>edad2){
+  console.log("usted es mayor de edad")
+}
+else{
+  console.log("usted es menor de edad")
+}
+
